db8bcc26baa303a9cb0ba7cedac5b398b737cab4
Becker Mathie <bmathie@mpr.org>
2022-07-01 09:58:54 -0500

    Basic todo

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,15 +1,24 @@
 <script lang="ts">
 import TodoListItem from "./TodoListItem.vue";
+import mockTodos from "../data/todos.json";
 
 export default {
-  props: {},
   components: {
     TodoListItem,
+  },
+  data: () => {
+    return {
+      todos: mockTodos,
+    };
   },
 };
 </script>
 
 <template>
-  <h1 class="text-center bg-color">Todo List</h1>
-  <TodoListItem />
+  <h1 class="p-10 text-xl font-bold text-center">Todo List</h1>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      <TodoListItem :todo="todo" :key="todo.id" />
+    </li>
+  </ul>
 </template>
--- src/components/TodoListItem.vue
+++ src/components/TodoListItem.vue
@@ -1,8 +1,14 @@
 <script lang="ts">
 export default {
-  props: {},
+  props: {
+    todo: Object,
+  },
 };
 </script>
 <template>
-  <h2 class="text-center bg-blue-600">Todo List Item</h2>
+  <div class="flex justify-center p-1 text-lg align-middle">
+    <input class="p-2" type="checkbox" />
+    <p class="p-2">{{ todo.title }}</p>
+    <button class="p-2">x</button>
+  </div>
 </template>
